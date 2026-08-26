0886759cfddb2b4f6e017d45fbb9a46017540519
jpcastroneves <jpcastroneves@travelwithnoma.com>
2026-08-26 10:33:21 -0300

    chore(onboardings): remove dead heartbeat/config-doc steps
    Both steps were wrapped in a triple-quoted string literal and never ran;
    SchdOnboardings itself is never instantiated anywhere in the repo. Same
    dead-code pattern already removed from noma_onboardings.py.

--- package/schd/handlers/schd_onboardings.py
+++ package/schd/handlers/schd_onboardings.py
@@ -489,38 +489,6 @@ class SchdOnboardings:
         
         
         
-        # It takes more than just creating a team-tool-org relationship to activate the tool. 
-        # You also need to create the job_docs and the config_doc for every org.
-        # If we are installing this tool in an existing Portfolio with existing orgs, we would need
-        # to run steps 9 and 10 in every org in that portfolio. The problem is that no orgs 
-        # are assigned to this tool in this case as the user needs to do it manually. 
-        # A Solution would be to run a check every time a team-tool-org rel is created (but that happens much after this onboarding script is run)
-        # What we could do here is to skip these steps if it is a installation on an existing portfolio.
-        
-        '''  
-        # Step 9: Create the job documents       
-        jobs = [{
-                "description": "This job is called to execute periodic operational tasks",
-                "handler": "schd/heartbeat",
-                "name": "Heartbeat",
-                "status": "available",
-                "type": "schd",
-                "version": "1.0.0"
-            }]
-    
-        response_9 = self.create_job_docs(self.bridge['portfolio_id'], self.bridge['org_id'],jobs) 
-        results.append(response_9)
-        if not response_9['success']: return {'success':False,'output':results}
-        '''
-    
-        '''
-        # Step 10: Create the config singleton document
-        response_10 = self.create_config_doc(self.bridge['portfolio_id'], self.bridge['org_id'],payload,'schd_config')
-        results.append(response_10)
-        if not response_10['success']: return {'success':False,'output':results}
-        '''
-        
-        
         # Step 11: Refresh the tree
         response_11 = self.refresh_tree()
         results.append(response_11)
